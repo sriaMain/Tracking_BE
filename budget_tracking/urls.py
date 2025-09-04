3d160de4b7863c963077d7581f8d6f5884ed7a9b
sridevi-sriagithub <sridevi@sriainfotech.com>
2025-09-04 18:11:45 +0530

    add rout

--- budget_tracking/urls.py
+++ budget_tracking/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("django backend is live on server")
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('login/', include('login.urls')), 
     path('organization/', include('organization.urls')),  
