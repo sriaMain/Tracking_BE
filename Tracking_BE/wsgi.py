0b0854641bc5530e2b57e94b895010ba48df4622
Teerdaveni <teerdaveni@sriainfotech.com>
2025-09-29 16:10:04 +0530

    Add automatic superuser creation on startup

--- Tracking_BE/wsgi.py
+++ Tracking_BE/wsgi.py
@@ -8,9 +8,33 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import django
 
 from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tracking_BE.settings')
+
+# application = get_wsgi_application()
+# django.setup()
+
+# if os.environ.get("CREATE_SUPERUSER_ON_STARTUP", "True") == "True":
+#     try:
+#         from create_superuser import create_superadmin
+#         create_superadmin()
+#     except Exception as e:
+#         print("Superuser creation failed:", e)
+
+# import os
+# from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tracking_BE.settings')
 
 application = get_wsgi_application()
+
+# Automatically create superuser on startup
+if os.environ.get("CREATE_SUPERUSER_ON_STARTUP", "True") == "True":
+    try:
+        from create_superuser import create_superadmin
+        create_superadmin()
+    except Exception as e:
+        print("Superuser creation failed:", e)
--- create_superuser.py
+++ create_superuser.py
@@ -1,19 +1,21 @@
-import os
-import django
+# create_superuser.py
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Tracking_BE.settings")
-django.setup()
+def create_superadmin():
+    import os
+    import django
 
-from django.contrib.auth import get_user_model
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Tracking_BE.settings")
+    django.setup()
 
-User = get_user_model()
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
 
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser(
-        username="Teerdaveni",
-        email="teerdaveni@sriainfotech.com",
-        password="Teerdaveni@2025"
-    )
-    print("Superuser created!")
-else:
-    print("Superuser already exists.")
+    if not User.objects.filter(username="Teerdaveni").exists():
+        User.objects.create_superuser(
+            username="Teerdaveni",
+            email="teerdaveni@sriainfotech.com",
+            password="Teerdaveni@2025"
+        )
+        print("Superuser created!")
+    else:
+        print("Superuser already exists.")
