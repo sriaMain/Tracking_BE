4a7548fcce12b31c9d6b79a783d892b58289272e
Teerdaveni <teerdaveni@sriainfotech.com>
2025-10-02 21:36:19 +0530

    smtp

--- Tracking_BE/settings.py
+++ Tracking_BE/settings.py
@@ -259,7 +259,7 @@ AUTH_USER_MODEL = 'login.User'
 import os
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.sendgrid.net')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
 EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL', 'False') == 'True'
